c4ed5560e9cf41f4e0dcbb2cca4d7452b176508f
Rob <roberto.carmeli@gmail.com>
2026-09-10 13:30:14 +0200

    Initial Commit

---
+++ Whisperer/WhispererApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhispererApp.swift
+//  Whisperer
+//
+//  Created by Roberto on 10.09.26.
+//
+
+import SwiftUI
+
+@main
+struct WhispererApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
